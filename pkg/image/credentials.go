d9d40eb3c5c5334b14239a8406739d9718add44d
Manuel Lorenzo <mlorenzofr@redhat.com>
2024-08-01 13:48:15 +0200

    Change credentials workflow

--- pkg/image/credentials.go
+++ pkg/image/credentials.go
@@ -1,11 +1,6 @@
 package image
 
 import (
-	"errors"
-	"os"
-	"path/filepath"
-
-	dockercfg "github.com/docker/cli/cli/config"
 	"github.com/openshift/library-go/pkg/image/registryclient"
 	"github.com/openshift/oc/pkg/cli/image/manifest/dockercredentials"
 	"k8s.io/client-go/rest"
@@ -25,28 +20,12 @@ func NewContext(skipVerification bool) (*registryclient.Context, error) {
 
 	ctx := registryclient.NewContext(rt, insecureRT)
 
-	// Set default options
-	var registryConfig string
-	dockerConfigJSON := filepath.Join(dockercfg.Dir(), dockercfg.ConfigFileName)
-	switch _, err := os.Stat(dockerConfigJSON); {
-	case err == nil:
-		registryConfig = dockerConfigJSON
-	case errors.Is(err, os.ErrNotExist):
-		podmanConfig := filepath.Join(os.Getenv("XDG_RUNTIME_DIR"), "containers/auth.json")
-		if _, err := os.Stat(podmanConfig); err == nil {
-			registryConfig = podmanConfig
-		} else if !os.IsNotExist(err) {
-			return nil, err
-		}
+	creds, err := dockercredentials.NewCredentialStoreFactory("")
+	if err != nil {
+		return nil, err
 	}
+	ctx.WithCredentialsFactory(creds)
 
-	if len(registryConfig) != 0 {
-		creds, err := dockercredentials.NewCredentialStoreFactory(registryConfig)
-		if err != nil {
-			return nil, err
-		}
-		ctx.WithCredentialsFactory(creds)
-	}
 	ctx.Retries = 3
 	ctx.DisableDigestVerification = skipVerification
 	return ctx, nil
